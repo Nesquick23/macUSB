c32458127afbcc5fbff1c12d112a7ed0d119fbb4
Krystian Pierz <krystianpierz@icloud.com>
2025-12-01 22:21:25 +0100

    Initial Commit

---
+++ macUSBTests/macUSBTests.swift
@@ -0,0 +1,17 @@
+//
+//  macUSBTests.swift
+//  macUSBTests
+//
+//  Created by Krystian Pierz on 01/12/2025.
+//
+
+import Testing
+@testable import macUSB
+
+struct macUSBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
